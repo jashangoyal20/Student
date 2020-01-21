18b59063042b038e5d9a191dc7df6078778210df
JASHAN GOYAL <JASHAN GOYAL@DESKTOP-33K917U>
2020-01-21 16:18:30 -0500

    commit4

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,6 +12,10 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student {
     
     private String name;
+    private String email;
+    
+    
+    
 
     /**
      * Get the value of name
